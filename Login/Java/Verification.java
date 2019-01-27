5c2dff8010dab534bf1e404adb11aaa36e5008ca
diegocruz10 <diacruzmo@unal.edu.co>
2019-01-27 09:57:00 -0500

    New Changes
    .

---
+++ Login/Java/Verification.java
@@ -0,0 +1,48 @@
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.FileInputStream;
+
+public class Verification 
+{
+    public static void main(String[] args) 
+    {
+        HttpClient httpclient = HttpClients.createDefault();
+		String verificationProfileId = "set-verification-profile-id-user";
+
+
+        try
+        {
+            URIBuilder builder = new URIBuilder(
+				"https://westus.api.cognitive.microsoft.com/spid/v1.0/verify?verificationProfileId="
+					+ verificationProfileId);
+
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/octet-stream");
+            request.setHeader("Ocp-Apim-Subscription-Key", "set-key");
+
+			request.setEntity(new InputStreamEntity(new FileInputStream("set-audio-wav")));
+
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) 
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
